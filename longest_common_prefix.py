6d98f4b8f0fe58a2ec2b030d9383f60690f19c9c
Moulyagowda-19 <168709974+Moulyagowda-19@users.noreply.github.com>
2026-04-11 22:25:24 +0530

    Create longest_common_prefix.py

---
+++ longest_common_prefix.py
@@ -0,0 +1,12 @@
+def longestCommonPrefix(self, strs):
+        if not strs:
+            return
+        prefix = strs[0]
+        for string in strs[1:]:
+            while string[:len(prefix)] != prefix and prefix:
+                prefix = prefix[:len(prefix)-1]
+        
+            if not prefix:
+               break
+
+        return prefix
