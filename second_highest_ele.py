63586f4d11b761c4a08941fba6941357ac25b3a9
Moulyagowda-19 <168709974+Moulyagowda-19@users.noreply.github.com>
2026-04-12 21:54:42 +0530

    Create second_highest_ele.py

---
+++ second_highest_ele.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def secondLargest(self, arr):
+        first = second = float('-inf')
+
+        for num in arr:
+            if num > first:
+                second = first
+                first = num
+            elif num > second and num != first:
+                second = num
+
+        return second
