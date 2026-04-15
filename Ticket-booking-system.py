e61d6fbcd5947e285b090dbfe5974f0475f79625
Moulyagowda-19 <168709974+Moulyagowda-19@users.noreply.github.com>
2026-04-15 22:38:43 +0530

    Implement basic ticket booking system

---
+++ Ticket-booking-system.py
@@ -0,0 +1,19 @@
+# Ticket Booking System
+
+def ticket_booking_system(seats, requests):
+    print("Total Seats:", seats)
+    print("Booking Status:")
+
+    for i in range(len(requests)):
+        if seats > 0:
+            print("Booked")
+            seats -= 1
+        else:
+            print("Waiting List")
+
+# Main Program
+if __name__ == "__main__":
+    seats = 3
+    requests = [1, 1, 1, 1]
+
+    ticket_booking_system(seats, requests)
